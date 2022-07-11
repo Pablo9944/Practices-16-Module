15d67702e87bec0a754e04ef220939ee4128d346
Павел Юров <peyurov@gmail.com>
2022-07-11 07:51:59 +0300

    Добавьте файлы проекта.

---
+++ Practices 16 Module/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices_16_Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ PracticesTests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Practices_16_Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalReturnTrueValue()
+        {
+            Calculator c = new Calculator();
+            Assert.IsTrue(c.Additional(2, 2) == 4);
+
+        }
+        [Test]
+        public void SubtractionReturnSumValue()
+        {
+            Calculator c = new Calculator();
+            Assert.AreEqual(4, c.Subtraction(6, 2));
+
+        }
+        [Test]
+        public void MiltiplicationReturnSumValue()
+        {
+            Calculator c = new Calculator();
+            Assert.IsFalse(c.Miltiplication(2, 2) == 6);
+
+        }
+        [Test]
+        public void DivisionReturnSumValue()
+        {
+            Calculator c = new Calculator();
+            Assert.NotZero(c.Division(9, 3));
+
+        }
+    }
+}
